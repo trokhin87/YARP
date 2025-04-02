0951e4652e31a6efd96c4caddcebe9fc0bf32296
trokhin87 <trokhin87@gmail.com>
2025-04-03 00:09:00 +0300

    чето сделалось, но запросы не отправляются

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,62 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Yarp.ReverseProxy;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.Run();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureServices((context, services) =>
+                {
+                    services.AddReverseProxy()
+                        .LoadFromConfig(context.Configuration.GetSection("ReverseProxy"));
+
+                    // Настроим Swagger для отображения API
+                    services.AddEndpointsApiExplorer();
+                    services.AddSwaggerGen(c =>
+                    {
+                        c.SwaggerDoc("v1", new OpenApiInfo 
+                        { 
+                            Title = "YARP API", 
+                            Version = "v1" 
+                        });
+                    });
+
+                });
+
+                webBuilder.Configure(app =>
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI(c =>
+                    {
+                        // Swagger для Auth сервиса
+                        c.SwaggerEndpoint("/swagger/auth/v1/swagger.json", "Auth Service API");
+    
+                        // Swagger для Friend сервиса
+                        c.SwaggerEndpoint("/swagger/friend/v1/swagger.json", "Friend Service API");
+    
+                        // Добавьте RoutePrefix если нужно
+                        c.RoutePrefix = "swagger";
+                    });
+
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapReverseProxy();
+                    });
+                });
+            });
+}
