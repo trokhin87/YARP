065e81a8aa7e4e5ced029263651e80ae2c85183f
trokhin87 <trokhin87@gmail.com>
2025-04-07 14:19:43 +0300

    old

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,16 +2,10 @@
 
 var builder=WebApplication.CreateBuilder(args);
 
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddReverseProxy()
-        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy_dev"));
-}
-else
-{
     builder.Services.AddReverseProxy()
         .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-}
+
+
 
 var app = builder.Build();
     app.MapReverseProxy();
